dbce314c08e12be7762ff4a5d3b62ac1f233ee8e
Palash Udhwani <paludhwani@gmail.com>
2020-12-06 14:59:10 +0530

    Pending commits

---
+++ app.js
@@ -0,0 +1,10 @@
+var btntranslate = document.querySelector("#btn-translate");
+
+//read more on document 
+
+btntranslate.addEventListener("click",function clickEventHandler() {
+
+    console.log("clicked")
+})
+
+//read more on syntax ...events
