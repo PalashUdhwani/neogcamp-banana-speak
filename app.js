ee56909e3aae6b50437bdc808f9c9da92a6265f2
Palash Udhwani <paludhwani@gmail.com>
2020-12-09 21:48:24 +0530

    9/12 updates-1

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var btntranslate = document.querySelector("#btn-translate");
+var txtinp = document.querySelector("#txt-inp");
 
 //read more on document 
 
-btntranslate.addEventListener("click",function clickEventHandler() {
 
-    console.log("clicked")
-})
+
+function clickHandler() {
+        //browser events 
+        console.log("clicked");
+        console.log("input",txtinp.value);
+        
+    };
 
 //read more on syntax ...events
+
+btntranslate.addEventListener("click",clickHandler())
